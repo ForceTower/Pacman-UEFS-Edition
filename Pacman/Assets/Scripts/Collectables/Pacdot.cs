58f233e13485cf2b426f80628589efde827e53ea
João Paulo <joaopaulo761@gmail.com>
2017-09-09 12:34:22 -0300

    Pacdots and Player Movement

--- Pacman/Assets/Scripts/Collectables/Pacdot.cs
+++ Pacman/Assets/Scripts/Collectables/Pacdot.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Picks : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class Pacdot : MonoBehaviour {
 
     private void OnTriggerEnter2D (Collider2D collision) {
         if (collision.tag == "Player") {
--- Pacman/Assets/Scripts/Player/PlayerController.cs
+++ Pacman/Assets/Scripts/Player/PlayerController.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float speed = 0.4f;
+    public Vector2 destination = Vector2.zero;
+    private new Rigidbody2D rigidbody;
+    private Animator animator;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D> ();
+        animator = GetComponent<Animator> ();
+        destination = transform.position;
+    }
+
+    private void FixedUpdate () {
+        Vector2 move = Vector2.MoveTowards (transform.position, destination, speed);
+        rigidbody.MovePosition (move);
+
+        print (Vector2.Distance (destination, transform.position));
+
+        if (Vector2.Distance (destination, transform.position) < 0.00001f) {
+            if (Input.GetKey (KeyCode.UpArrow) && ValidDirection (Vector3.up))
+                destination = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey (KeyCode.DownArrow) && ValidDirection (Vector3.down))
+                destination = (Vector2)transform.position + Vector2.down;
+            if (Input.GetKey (KeyCode.LeftArrow) && ValidDirection (Vector3.left))
+                destination = (Vector2)transform.position + Vector2.left;
+            if (Input.GetKey (KeyCode.RightArrow) && ValidDirection (Vector3.right))
+                destination = (Vector2)transform.position + Vector2.right;
+        }
+
+        Vector2 direction = destination - (Vector2)transform.position;
+        animator.SetFloat ("Horizontal", direction.x);
+        animator.SetFloat ("Vertical",   direction.y);
+    }
+
+    private bool ValidDirection(Vector2 direction) {
+        Vector2 position = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast (position + direction, position);
+
+        //We just hit ourself
+        if (hit.collider == GetComponent<Collider2D> ()) {
+            print ("Valid");
+            return true;
+        }
+        //We hit something else
+        else {
+            print ("Invalid");
+            return false;
+        }
+    }
 }
